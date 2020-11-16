2fb7a8e27616e4e89d60bd3f530a0a7a45331cc3
Olaf Sulich <olafsulich@gmail.com>
2020-11-16 19:16:56 +0100

    Initial configuration

---
+++ pages/_app.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/globals.css';
+
+const isProduction = process.env.NODE_ENV === 'production';
+const isBrowser = typeof window !== 'undefined';
+
+if (!isProduction && isBrowser) {
+  const axe = require('react-axe');
+  const AXE_DELAY = 1000;
+  axe(React, ReactDOM, AXE_DELAY);
+}
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, user-scalable=yes, initial-scale=1.0, viewport-fit=cover"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="apple-mobile-web-app-title" content="DevFeedback" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
