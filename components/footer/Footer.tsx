94f1fb097e6cddfa354de4d69f0c1cdcb61c0966
Olaf Sulich <olafsulich@gmail.com>
2020-11-25 16:26:05 +0100

    Create footer

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,23 @@
+import { memo } from 'react';
+import styles from './footer.module.scss';
+import NewsletterForm from '../header/shared/components/newsletterForm/NewsletterForm';
+import SocialLinks from '../header/shared/components/socialLinks/SocialLinks';
+
+const Footer = () => {
+  return (
+    <footer className={styles.wrapper}>
+      <div className={styles.newsletterWrapper}>
+        <h2 className={styles.heading}>Newsletter</h2>
+        <p className={styles.text}>
+          Pewnie spytasz, czemu miałbym się zapisywać na kolejny newsletter? Przecież jestem
+          zapisany już dox newsletterów!!! Ja tak samo i tak samo jak Ty, nie lubię spamu i zalewu
+          jaki dostaję na maila.
+        </p>
+        <NewsletterForm />
+        <SocialLinks />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { memo, ReactNode } from 'react';
 import Header from '../header/Header';
+import Footer from 'components/footer/Footer';
 import styles from './layout.module.scss';
 
 type LayoutProps = { children: ReactNode };
@@ -8,7 +9,8 @@ const Layout = memo<LayoutProps>(({ children }) => {
   return (
     <div className={styles.wrapper}>
       <Header />
-      {children}
+      <main>{children}</main>
+      <Footer />
     </div>
   );
 });
