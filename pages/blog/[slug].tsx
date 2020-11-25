9133daab68279e9ddbfb284edb4a9a2d073b5cfe
Olaf Sulich <olafsulich@gmail.com>
2020-11-25 21:06:42 +0100

    Add mdx-prism

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,6 +1,11 @@
 import matter from 'gray-matter';
 import hydrate from 'next-mdx-remote/hydrate';
 import renderToString from 'next-mdx-remote/render-to-string';
+import remarkAutoLinkHeadings from 'remark-autolink-headings';
+import remarkSlug from 'remark-slug';
+import remarkCodeTitles from 'remark-code-titles';
+import mdxPrism from 'mdx-prism';
+import readingTime from 'reading-time';
 import path from 'path';
 import fs from 'fs';
 import { postFilePaths, POSTS_PATH } from 'lib/mdx';
@@ -11,7 +16,7 @@ const components = {
 
 const BlogPost = ({ source, frontmatter }: any) => {
   const content = hydrate(source, { components });
-
+  console.log(frontmatter);
   return (
     <div>
       <h1>frontmatter.title</h1>
@@ -31,8 +36,14 @@ export const getStaticProps = async ({ params }: any) => {
   const mdxSource = await renderToString(content, {
     components,
     mdxOptions: {
-      remarkPlugins: [],
-      rehypePlugins: [],
+      remarkPlugins: [remarkAutoLinkHeadings, remarkSlug, remarkCodeTitles],
+      rehypePlugins: [mdxPrism],
+      extendFrontMatter: {
+        process: (mdxContent: string) => ({
+          wordCount: mdxContent.split(/\s+/gu).length,
+          readingTime: readingTime(mdxContent),
+        }),
+      },
     },
     scope: data,
   });
