9d464bde21522a137cbc6a45c5eeb62c53adfa0f
Olaf Sulich <olafsulich@gmail.com>
2020-11-25 17:43:09 +0100

    Create blog page

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -3,16 +3,17 @@ import Layout from '../components/layout/Layout';
 import React from 'react';
 import Navigation from '../components/navigation/Navigation';
 import Heading from 'components/shared/components/heading/Heading';
+import styles from './blog.module.scss';
 
 export default function Blog() {
   return (
     <Layout>
       <Navigation />
-      <main>
+      <main className={styles.wrapper}>
         <PostsListing>
-          <Heading tag="h1" variant="primary">
+          <Heading tag="h1" variant="primary" className={styles.heading}>
             Artykuły
-        </Heading>
+          </Heading>
         </PostsListing>
       </main>
     </Layout>
