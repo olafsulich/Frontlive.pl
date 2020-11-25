a8b98bfa09f5c42f6df99b9600e4820417500afd
Olaf Sulich <olafsulich@gmail.com>
2020-11-25 15:43:34 +0100

    Create workshop card

---
+++ components/workshop/Workshop.tsx
@@ -0,0 +1,35 @@
+import { memo } from 'react';
+import styles from './workshop.module.scss';
+
+const Workshop = memo(() => {
+  return (
+    <article className={styles.wrapper}>
+      <div className={styles.textWrapper}>
+        <div className={styles.info}>
+          <h2 className={styles.heading}>
+            <img src="/images/przeprogramowani.png" alt="" className={styles.image} />
+            <span className={styles.headingText}>Kurs Gatsby.js</span>
+          </h2>
+        </div>
+        <p className={styles.text}>
+          Earlier this year, I developed Cubital Tunnel Syndrome, a repetitive-strain injury, in
+          both of my elbows. As a result.
+        </p>
+        <div className={styles.linkWrapper}>
+          <a
+            href="https://przeprogramowani.pl/darmowy-kurs-gatsby"
+            target="_blank"
+            rel="noopener noreferer"
+            className={styles.link}
+          >
+            <span className="visually-hidden">Czytaj</span>
+            <span>Więcej</span>
+          </a>
+        </div>
+      </div>
+      <img src="/images/gatsby-cta.png" alt="" className={styles.logo} />
+    </article>
+  );
+});
+
+export default Workshop;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Header from '../components/header/Header';
 import PostsListing from '../components/postsListing/PostsListing';
 import Layout from '../components/layout/Layout';
 import Courses from '../components/courses/Courses';
+import Workshop from '../components/workshop/Workshop';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <PostsListing heading="Najnowsze" />
       <Courses />
       <PostsListing heading="Najpopularniejsze" />
+      <Workshop />
     </Layout>
   );
 }
