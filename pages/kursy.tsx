b25a6d66e627deb970b3749421e5685b41dfdd5a
Olaf Sulich <olafsulich@gmail.com>
2020-11-25 17:46:54 +0100

    Create courses page

---
+++ pages/kursy.tsx
@@ -0,0 +1,20 @@
+import Layout from '../components/layout/Layout';
+import React from 'react';
+import Navigation from '../components/navigation/Navigation';
+import Heading from 'components/shared/components/heading/Heading';
+import styles from './kursy.module.scss';
+import Courses from 'components/courses/Courses';
+
+export default function Kursy() {
+  return (
+    <Layout>
+      <Navigation />
+      <main className={styles.wrapper}>
+        <Heading tag="h1" variant="primary" className={styles.heading}>
+          Kursy
+        </Heading>
+        <Courses />
+      </main>
+    </Layout>
+  );
+}
