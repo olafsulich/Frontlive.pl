cbe707aa844a5bcafa1ab7824aa00df0d209fd1c
Olaf Sulich <olafsulich@gmail.com>
2020-11-26 18:46:54 +0100

    Add types to mdx components

--- components/mdx/MdxProvider.tsx
+++ components/mdx/MdxProvider.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react';
+import { ReactNode } from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import Heading from './heading/Heading';
 import Paragraph from './paragraph/Paragraph';
@@ -10,25 +10,31 @@ import Pre from './pre/Pre';
 import Code from './code/Code';
 import Em from './em/Em';
 
-const components = {
-  h2: (props: any) => <Heading tag="h2" {...props} />,
-  h3: (props: any) => <Heading tag="h3" {...props} />,
-  h4: (props: any) => <Heading tag="h4" {...props} />,
-  h5: (props: any) => <Heading tag="h5" {...props} />,
-  h6: (props: any) => <Heading tag="h6" {...props} />,
-  p: (props: any) => <Paragraph {...props} />,
-  a: (props: any) => <Link href={props.href} {...props} />,
-  blockquote: (props: any) => <Blockquote {...props} />,
-  ul: (props: any) => <Ul {...props} />,
-  ol: (props: any) => <Ol {...props} />,
-  pre: (props: any) => <Pre {...props} />,
-  code: (props: any) => <Code {...props} />,
-  em: (props: any) => <Em {...props} />,
+type ComponentProps = {
+  children: ReactNode;
 };
 
-type MdxCompProviderProps = { children: ReactNode };
+type LinkComponentProps = {
+  href: string;
+} & ComponentProps;
 
-const MdxCompProvider = ({ children }: MdxCompProviderProps) => {
+const components = {
+  h2: (props: ComponentProps) => <Heading tag="h2" {...props} />,
+  h3: (props: ComponentProps) => <Heading tag="h3" {...props} />,
+  h4: (props: ComponentProps) => <Heading tag="h4" {...props} />,
+  h5: (props: ComponentProps) => <Heading tag="h5" {...props} />,
+  h6: (props: ComponentProps) => <Heading tag="h6" {...props} />,
+  p: (props: ComponentProps) => <Paragraph {...props} />,
+  a: (props: LinkComponentProps) => <Link {...props} />,
+  blockquote: (props: ComponentProps) => <Blockquote {...props} />,
+  ul: (props: ComponentProps) => <Ul {...props} />,
+  ol: (props: ComponentProps) => <Ol {...props} />,
+  pre: (props: ComponentProps) => <Pre {...props} />,
+  code: (props: ComponentProps) => <Code {...props} />,
+  em: (props: ComponentProps) => <Em {...props} />,
+};
+
+const MdxCompProvider = ({ children }: ComponentProps) => {
   return <MDXProvider components={components}>{children}</MDXProvider>;
 };
 
