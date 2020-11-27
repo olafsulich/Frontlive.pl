068af289c63ca30aebc8e69e227faee298846f85
Olaf Sulich <olafsulich@gmail.com>
2020-11-27 03:57:14 +0100

    Add links to social icons

--- components/shared/components/socialLinks/SocialLinks.tsx
+++ components/shared/components/socialLinks/SocialLinks.tsx
@@ -6,9 +6,33 @@ import TwitterIcon from '../../../../public/icons/twitter.svg';
 const SocialLinks = () => {
   return (
     <div className={styles.wrapper}>
-      <GitHubIcon className={styles.icon} />
-      <LinkedInIcon className={styles.icon} />
-      <TwitterIcon className={styles.icon} />
+      <a
+        href="https://github.com/olafsulich"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.link}
+      >
+        <span className="visually-hidden">Repozytorium na GitHubie</span>
+        <GitHubIcon className={styles.icon} />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/olaf-sulich"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.link}
+      >
+        <span className="visually-hidden">Profil LinkedIn</span>
+        <LinkedInIcon className={styles.icon} />
+      </a>
+      <a
+        href="https://twitter.com/sulich_olaf"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.link}
+      >
+        <span className="visually-hidden">Konto na Twitterze</span>
+        <TwitterIcon className={styles.icon} />
+      </a>
     </div>
   );
 };
