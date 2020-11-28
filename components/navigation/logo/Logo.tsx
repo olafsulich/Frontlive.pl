5f976d4f1c1d5c18fcd3088da90ccf8fb51f1cec
Olaf Sulich <olafsulich@gmail.com>
2020-11-29 00:30:15 +0100

    Change styles of navigation link

--- components/navigation/logo/Logo.tsx
+++ components/navigation/logo/Logo.tsx
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 const Logo = () => {
-  return <img src="/images/logo.png" alt="" />;
+  return (
+    <Link href="/">
+      <a>
+        <img src="/images/logo.png" alt="" />
+        <span className="visually-hidden">Frontlive.pl</span>
+      </a>
+    </Link>
+  );
 };
 
 export default Logo;
