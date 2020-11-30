70e36655195c9f7490ce6d609459992ab7203504
Olaf Sulich <olafsulich@gmail.com>
2020-11-30 12:37:30 +0100

    Add basic styles for technical styles list

--- components/skillsListing/technicalSkills/TechnicalSkills.tsx
+++ components/skillsListing/technicalSkills/TechnicalSkills.tsx
@@ -2,10 +2,13 @@ import { memo } from 'react';
 import { frontendSkills, backendSkills, devOpsSkills, otherSkills } from '../../../data/skills';
 import styles from './technicalSkills.module.scss';
 import TechnicalSkillsList from './technicalSkillsList/TechnicalSkillsList';
+import Heading from '../../shared/components/heading/Heading';
 
 const TechnicalSkills = memo(() => (
   <section className={styles.wrapper}>
-    <h2 className={styles.heading}>Umiejętności techniczne</h2>
+    <Heading variant="secondary" tag="h2" className={styles.heading}>
+      Umiejętności techniczne
+    </Heading>
     <div className={styles.skills}>
       <TechnicalSkillsList title="Frontend" skills={frontendSkills} variant="blue" />
       <TechnicalSkillsList title="Backend" skills={backendSkills} variant="green" />
