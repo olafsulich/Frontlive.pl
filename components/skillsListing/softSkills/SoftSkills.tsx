bc2feebec9aa729dfa3ca428accb78cd0f9b49f4
Olaf Sulich <olafsulich@gmail.com>
2020-11-30 13:05:16 +0100

    Add basic styles for soft skills

--- components/skillsListing/softSkills/SoftSkills.tsx
+++ components/skillsListing/softSkills/SoftSkills.tsx
@@ -2,10 +2,13 @@ import { memo } from 'react';
 import { softSkills } from '../../../data/skills';
 import styles from './softSkills.module.scss';
 import SoftSkillsList from './softSkillsList/SoftSkillsList';
+import Heading from '../../shared/components/heading/Heading';
 
 const SoftSkills = memo(() => (
   <section className={styles.wrapper}>
-    <h2 className={styles.heading}>Umiejętności miękkie</h2>
+    <Heading variant="secondary" tag="h2" className={styles.heading}>
+      Umiejętności miękkie
+    </Heading>
     <div className={styles.skills}>
       <SoftSkillsList title="Umiejętności miękkie" skills={softSkills} />
     </div>
