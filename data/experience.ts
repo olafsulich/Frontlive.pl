8d9f5c59d580e13aefa87fd88f4a0a6118095c6a
Olaf Sulich <olafsulich@gmail.com>
2020-11-30 12:20:20 +0100

    Add basic styles for experience card

--- data/experience.ts
+++ data/experience.ts
@@ -11,20 +11,20 @@ export const experience = [
     description:
       'I recently got the following message in a DM: I’ve been building with HTML and CSS for years, and I still don’t know how to implement pixel-perfect designs',
     duration: 'listopad 2020 - obecnie',
-    image: '/images/logo.png',
+    image: '/images/przeprogramowani-logo.png',
   },
   {
     title: 'Freelancer - Hypnocoders',
     description:
       'I recently got the following message in a DM: I’ve been building with HTML and CSS for years, and I still don’t know how to implement pixel-perfect designs',
     duration: 'marzec 2020 - obecnie',
-    image: '/images/logo.png',
+    image: '/images/hypnocoders-logo.png',
   },
   {
     title: 'Frontend Developer - BlackSoft',
     description:
       'I recently got the following message in a DM: I’ve been building with HTML and CSS for years, and I still don’t know how to implement pixel-perfect designs',
     duration: 'wrzesień 2019',
-    image: '/images/logo.png',
+    image: '/images/blacksoft-logo.png',
   },
 ];
