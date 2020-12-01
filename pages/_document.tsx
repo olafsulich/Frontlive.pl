ed00a0a6f3dfe3c42bf4a3f60c535a9396910a8c
Olaf Sulich <olafsulich@gmail.com>
2020-12-01 15:48:31 +0100

    Fix custom fonts and favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,8 +15,10 @@ class MyDocument extends Document {
         itemType="http://schema.org/WebPage"
       >
         <Head>
-          <link rel="preload" href="/fonts/CriteriaCFBold.otf" as="font" crossOrigin="" />
-          <link rel="preload" href="/fonts/CriteriaCFDemibold.otf" as="font" crossOrigin="" />
+          <link rel="preload" href="/fonts/CriteriaCFBold.woff" as="font" crossOrigin="" />
+          <link rel="preload" href="/fonts/CriteriaCFDemibold.woff" as="font" crossOrigin="" />
+          <link rel="preload" href="/fonts/CriteriaCFBold.ttf" as="font" crossOrigin="" />
+          <link rel="preload" href="/fonts/CriteriaCFDemibold.ttf" as="font" crossOrigin="" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             rel="preload"
@@ -28,25 +30,14 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@600&display=swap"
             as="stylesheet"
           />
-          <link rel="apple-touch-icon" sizes="57x57" href="favicons/apple-icon-57x57.png" />
-          <link rel="apple-touch-icon" sizes="60x60" href="favicons/apple-icon-60x60.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="favicons/apple-icon-72x72.png" />
-          <link rel="apple-touch-icon" sizes="76x76" href="favicons/apple-icon-76x76.png" />
-          <link rel="apple-touch-icon" sizes="114x114" href="favicons/apple-icon-114x114.png" />
-          <link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-icon-120x120.png" />
-          <link rel="apple-touch-icon" sizes="144x144" href="favicons/apple-icon-144x144.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="favicons/apple-icon-152x152.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-icon-180x180.png" />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="192x192"
-            href="favicons/android-icon-192x192.png"
-          />
+          <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="96x96" href="favicons/favicon-96x96.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
-          <meta name="apple-mobile-web-app-title" content="DevFeedback" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#000000" />
+          <meta name="msapplication-TileColor" content="#000000" />
+          <meta name="theme-color" content="#000000" />
+          <meta name="apple-mobile-web-app-title" content="Frontlive" />
         </Head>
         <body>
           <Main />
