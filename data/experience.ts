7df7e4a904a13f8214ccd8df8795002e7c57d421
Olaf Sulich <olafsulich@gmail.com>
2020-12-04 00:47:51 +0100

    Update experience

--- data/experience.ts
+++ data/experience.ts
@@ -2,28 +2,28 @@ export const experience = [
   {
     title: 'Bloger - Frontlive.pl',
     description:
-      'I recently got the following message in a DM: I’ve been building with HTML and CSS for years, and I still don’t know how to implement pixel-perfect designs',
+      'Dziele się wiedzą i piszę o szeroko pojętym frontendzie. Cały blog zorientowany jest wokół React.js - mojej głównej technologii, ale znajdziesz tutaj wiele więcej niż to. Dostępność, TypeScript, React, GraphQL to tylko niektóre z zagadnień, o których piszę w swoich artykułach.',
     duration: 'marzec 2020 - obecnie',
     image: '/images/logo.png',
   },
   {
     title: 'Bloger - Przeprogramowani',
     description:
-      'I recently got the following message in a DM: I’ve been building with HTML and CSS for years, and I still don’t know how to implement pixel-perfect designs',
+      'Przeprogramowani to grupa, której celem jest budować świadomych i gotowych na wyzwania tego zawodu specjalistów. W tym co robimy stawiamy zawsze na sprawdzone informacje poparte badaniami, naukę od osób bardziej doświadczonych od nas oraz na proces nieustannego rozwoju. Działamy na Youtube, blogu i prowadzimy programistyczne podcasty.',
     duration: 'listopad 2020 - obecnie',
     image: '/images/przeprogramowani-logo.png',
   },
   {
     title: 'Freelancer - Hypnocoders',
     description:
-      'I recently got the following message in a DM: I’ve been building with HTML and CSS for years, and I still don’t know how to implement pixel-perfect designs',
+      'Hypnocoders to grupa 5 programistów - freelancerów. Działamy głównie z klientami z Holandii, dla których realizujemy najróżniejsze projekty komercyjne. Pracujemy głównie z React.js i Node. Specjalizujemy się w JamStacku.',
     duration: 'marzec 2020 - obecnie',
     image: '/images/hypnocoders-logo.png',
   },
   {
     title: 'Frontend Developer - BlackSoft',
     description:
-      'I recently got the following message in a DM: I’ve been building with HTML and CSS for years, and I still don’t know how to implement pixel-perfect designs',
+      'Miesięczna praktyka w BlackSoft była moim pierwszym doświadczonym komercyjnym. Przez ten czas pracowałem w 4 osobowym zespole, gdzie wspólnymi siłami realizowaliśmy nowe projekty, głównie na platformie WordPress.',
     duration: 'wrzesień 2019',
     image: '/images/blacksoft-logo.png',
   },
