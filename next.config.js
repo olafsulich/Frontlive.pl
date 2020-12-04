3a71026e86cb793bf2f3b567f7c6ccee33471749
Olaf Sulich <olafsulich@gmail.com>
2020-12-04 21:37:34 +0100

    Remove unused deps

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const withOptimizedImages = require('next-optimized-images');
-const withOffline = require('next-offline');
-
 require('what-input');
 
 const withPolyfills = (module.exports = (nextConfig = {}) => {
@@ -27,48 +25,12 @@ const withPolyfills = (module.exports = (nextConfig = {}) => {
   });
 });
 
-module.exports = withOffline(
+module.exports = withPolyfills(
   withOptimizedImages({
     imagesFolder: 'images',
 
-    workboxOpts: {
-      swDest: process.env.NEXT_EXPORT ? 'service-worker.js' : 'static/service-worker.js',
-      runtimeCaching: [
-        // {
-        //   urlPattern: /\.(?:png|jpg|jpeg|svg|webp)$/,
-        //   handler: 'CacheFirst',
-        //   options: {
-        //     cacheName: 'images',
-        //     expiration: {
-        //       maxEntries: 3,
-        //     },
-        //   },
-        // },
-        {
-          urlPattern: /^https?.*api/,
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'offlineCache',
-            expiration: {
-              maxEntries: 50,
-              maxAgeSeconds: 30 * 24 * 60 * 60,
-              purgeOnQuotaError: true,
-            },
-          },
-        },
-      ],
-    },
-
     sassOptions: {
       includePaths: [path.join(__dirname, 'styles')],
-    },
-    async rewrites() {
-      return [
-        {
-          source: '/service-worker.js',
-          destination: '/_next/static/service-worker.js',
-        },
-      ];
     },
   }),
 );
