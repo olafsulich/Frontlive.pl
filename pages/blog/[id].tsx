b5c02b1390c2f35491aaec2864d2090b5694f356
Olaf Sulich <olafsulich@gmail.com>
2022-03-28 21:29:08 +0200

    fix: build

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,11 +1,7 @@
-import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { InferGetStaticPropsType } from 'next';
 import { NextSeo } from 'next-seo';
-import { getMDXComponent } from 'mdx-bundler/client';
-import { useMemo } from 'react';
 import { Header } from '../../components/layout/header/header';
-import { Image } from '../../components/mdx/image/image';
 import { Footer } from '../../components/layout/footer/footer';
-import { getPostBySlug } from '../../lib/posts';
 import { getPost } from '../../lib/mdx';
 import { MDX } from '../../components/mdx/mdx';
 
@@ -17,25 +13,26 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps() {
-  const { code,frontmatter } = await getPost('test');
+  const { code, frontmatter } = await getPost('test');
 
   return {
     props: {
       code,
-      frontmatter
+      frontmatter,
     },
   };
 }
 
-export default function Index({ code,frontmatter }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Index({
+  code,
+  frontmatter,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
       <NextSeo />
       <div style={{ maxWidth: '1450px', margin: '0 auto' }}>
         <Header />
-        <main>
-          <MDX code={code} frontmatter={frontmatter} />
-        </main>
+        <main>{/* <MDX code={code} frontmatter={frontmatter} /> */}</main>
         <Footer />
       </div>
     </>
