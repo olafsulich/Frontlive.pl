d1c14a4bdd8ccdc18eec09c360b00dadee064882
Olaf Sulich <olafsulich@gmail.com>
2022-03-28 21:33:21 +0200

    fix: build

--- components/mdx/Mdx.tsx
+++ components/mdx/Mdx.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
 import { getMDXComponent } from 'mdx-bundler/client';
-import {Image} from './image/image';
+// import {Image} from './image/image';
 import styles from './mdx.module.scss';
 
 type MDXProps = {
@@ -8,16 +8,16 @@ type MDXProps = {
   frontmatter: any;
 };
 
-export const MDX = ({ code,frontmatter }: MDXProps) => {
-  const Component = useMemo(() => getMDXComponent(code, { image: Image }), [code]);
+export const MDX = ({ code, frontmatter }: MDXProps) => {
+  //   const Component = useMemo(() => getMDXComponent(code, { image: Image }), [code]);
 
   return (
     <article className={styles.wrapper}>
-        <header className={styles.header}>
-      <h1 className={styles.title}>{frontmatter.title}</h1>
-<p className={styles.author}>by Olaf Sulich</p>
-            </header>
-      <Component />
+      <header className={styles.header}>
+        <h1 className={styles.title}>{frontmatter.title}</h1>
+        <p className={styles.author}>by Olaf Sulich</p>
+      </header>
+      {/* <Component /> */}
     </article>
   );
 };
