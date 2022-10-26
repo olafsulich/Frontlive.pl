2a370ff2b93c7a0ccca6071a82b58148c8e13f99
Maciej Garncarski <60816511+MaciejGarncarski@users.noreply.github.com>
2022-10-26 19:54:33 +0200

    fix: favicons (#40)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -53,9 +53,9 @@ class MyDocument extends Document {
             type="font/woff2"
             crossOrigin=""
           />
-          <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
           <link rel="manifest" href="./site.webmanifest" />
           <link rel="alternate" title="rss feed" type="application/rss+xml" href="/rss.xml" />
 
